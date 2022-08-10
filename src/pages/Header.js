e3d0f0f342e73e76c84ed21bf3545c4e98e5414f
Captain Crypto <panamaworldwideinvestments@gmail.com>
2022-08-10 15:40:37 +0100

    Update Header.js

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+const MyProfilePicture = require("../assets/logos/Cap.png");
+
 function Header() {
   return (
     <div id="Header" className="mx-12 lg:mx-60 pt-60 items-center justify-center lg:w-1/2 text-justify">
@@ -10,7 +12,9 @@ function Header() {
       <a href="#projects"><button className="border border-lightblue_vs text-lightblue_vs mt-10 p-3 rounded hover:bg-opacity-10 hover:bg-lightblue_vs w-1/2">
         <code>Check out my Projects !</code>
       </button></a>
+     <img  src={MyProfilePicture} alt="Captain Crypto png"/> 
     </div>
+   
   );
 }
 
